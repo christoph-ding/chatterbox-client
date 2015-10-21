995c416a4a246fa19889fa96de63a6bd69249567
ironpup <christoph.ding@gmail.com>
2015-10-20 17:22:57 -0700

    Made entire website work.  All features in basic requirement present.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,18 +3,54 @@
 
 var app = {};
 
+
 $( document ).ready(function() {
 
   app.server = 'https://api.parse.com/1/classes/chatterbox';
+
   app.init = function(){
-    $('#send .submit').on('submit', function() {
-        app.handleSubmit();
-    });
   }
+
+  app.currentRoom = false;
+  app.currentFriend = false;
+
+  $('#send').on('click', function() {
+    app.handleSubmit();
+  });
 
   $('#refresh').on('click', function() {
       app.fetch();
-  })
+  });
+
+ $('#addRoom').on('click', function(){
+  app.addRoom($('#newRoomName').val());
+ });
+
+
+$('#roomSelect').change(function(){
+   
+   if( $('#roomSelect option:selected').text() === " All Rooms ") {
+    app.currentRoom = false;
+   } else {
+   app.currentRoom = $('#roomSelect option:selected').text();
+   }
+   $('#chats').empty();
+   app.fetch();
+});
+
+$('#friendSelect').change(function(){
+   
+   if( $('#friendSelect option:selected').text() === " Everyone ") {
+    app.currentFriend = false;
+   } else {
+   app.currentFriend = $('#friendSelect option:selected').text();
+   }
+   $('#chats').empty();
+   app.fetch();
+});
+
+
+  app.friendlist = {};
 
   app.send = function(message){
     $.ajax({
@@ -36,16 +72,52 @@ $( document ).ready(function() {
   app.fetch = function(){
     $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    url: app.server,
+    url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
     contentType: 'application/json',
     success: function (data) {
-      for (var i=0; i<data.results.length; i++) {
-        app.addMessage(app.convertForSafety(data.results[i]["text"]));
+      var rooms = {};
+      var results = data.results
+      for (var i=0; i< results.length; i++) {
+        var currentMessage = app.convertForSafety(results[i]["text"]);
+        var currentUser = app.convertForSafety(results[i]["username"]);
+        var messageRoom = app.convertForSafety(results[i]["roomname"]);
+
+        if (app.currentFriend === false) {
+          if (app.currentRoom === false) {
+            app.addMessage(results[i]);   
+            if (! (messageRoom in rooms) ) {
+              rooms[messageRoom] = true;
+            }   
+         } else {
+            if (messageRoom === app.currentRoom) {
+              app.addMessage(results[i]);             
+            }         
+          }          
+        } else {
+            if (currentUser === app.currentFriend) {
+               if (app.currentRoom === false) {
+                 app.addMessage(results[i]);   
+                 if (! (messageRoom in rooms) ) {
+                   rooms[messageRoom] = true;
+                 }   
+              } else {
+                 if (messageRoom === app.currentRoom) {
+                   app.addMessage(results[i]);             
+                 }         
+               }
+            }
+        }
       }
+
+      for(var room in rooms){
+          app.addRoom(room);
+      }
+
       console.log(data);
       console.log('chatterbox: Message fetched');
-    },
+    }
+  ,
     error: function (data) {
       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
       console.error('chatterbox: Failed to fetch message');
@@ -54,13 +126,19 @@ $( document ).ready(function() {
   }
 
   app.addMessage = function(message){
-    // $('#chats').after($("<li>" + message +"</li>")).addClass('username');
-    $("<li>" + message +"</li>").prependTo('#chats').addClass('username');
+    // $("<div></div>").append("<h4>"+ app.convertForSafety(message.username) +"</h4>")
+    // .append("<p>"+ app.convertForSafety(message.roomname) +"</p>")
+    // .append('<p>' + app.convertForSafety(message.text) + '</p>')
+    $("<div> <span> <button class='username'>" + app.convertForSafety(message.username) + "</button>:" + app.convertForSafety(message.text) + "</span></div>")
+    .prependTo('#chats');
 
-    $('.username').click(function(){
-      app.addFriend();
-    });
+   $('.username').on('click', function() {
+      app.addFriend($(this).text());
+   })
 
+    // $('.username').click(function(){
+    //   app.addFriend();
+    // });
   }
 
   app.clearMessages = function() {
@@ -68,16 +146,28 @@ $( document ).ready(function() {
   };
 
   app.addRoom = function(roomName) {
-    $('#roomSelect').append("<li>" + roomName + "</li>");
+    $('#roomSelect').append("<option>" + roomName + "</option>");
   };
 
-  app.addFriend = function(){
+  app.addFriend = function(friend){
+    if (!(friend in app.friendlist)) {
+      app.friendlist[friend] = true
+      $('#friendSelect').append("<option>" + friend + "</option>");
+    }
   };
 
 
   app.handleSubmit = function(){
-
-    console.log('hi');
+    var newMessage = {};
+    newMessage.username = window.location.search.slice(window.location.search.indexOf('=') + 1);
+    newMessage.text = $('#newMessage').val();
+    if (app.currentRoom === false) {
+      newMessage.roomname = "";
+    } else {
+      newMessage.roomname = app.currentRoom;
+    }
+    $('#newMessage').val("");
+    app.send(newMessage);
   };
 
 
@@ -85,10 +175,3 @@ $( document ).ready(function() {
     return _.escape(messageText);
   }
 });
-
-
-
-
-
-
-
